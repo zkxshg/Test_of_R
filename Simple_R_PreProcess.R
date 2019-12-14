92a28d83b6bc022181eaa9aa5e615cd65de8a92a
zkxshg <35519607+zkxshg@users.noreply.github.com>
2019-12-14 22:54:18 +0800

    Update Simple_R_PreProcess.R

--- Simple_R_PreProcess.R
+++ Simple_R_PreProcess.R
@@ -1,4 +1,22 @@
 # ====================1 常見library==============
+
 library(dplyr)  # 數據處理工具集
 library(fpp2)  # Forecast工具集
 library(nnet)  # 神經網絡工具集
+
+# 關聯規則及可視化
+library(arules) 
+library(arulesViz)  
+
+library(nutshell)  # data sets used as examples in the book "R in a Nutshell" 
+
+# 文本挖掘工具集
+library(tm)  # A framework for text mining applications
+library(wordcloud)  # 繪製詞雲
+library(RColorBrewer)  # 可視化修飾
+library(SnowballC)  # 詞幹提取
+library(topicmodels)  # 主題模型
+library(qdap)  # 文本量化分析
+
+library(e1071)  # 概率統計數據分析工具包
+library(rpart)  #
